326945aee82ec76749f1efd83a5b56fabc9cc868
Kamil Kadir <mkamil.akadir95@gmail.com>
2026-03-13 18:22:09 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+function toggleTheme(){
+document.body.classList.toggle("dark");
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+anchor.addEventListener("click", function(e){
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+behavior:"smooth"
+});
+
+});
+
+});
+
 function openDashboard(){
 document.getElementById("dashboardModal").style.display="block";
 }
